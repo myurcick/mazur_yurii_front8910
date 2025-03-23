fd1b6e26b92c8b6052132d17d084619560b6b6e0
Мазур Юрій bc2024-1 <yurii.mazur@lnu.edu.ua>
2025-03-23 19:53:14 +0200

    feat: integration components in main

--- cinema_proj/src/App.js
+++ cinema_proj/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.jsx
+import React from 'react';
+import MovieList from './components/MovieList';
+import { movies } from './data/movies';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>Кінопоказ Тижня</h1>
       </header>
+      <main>
+        <MovieList movies={movies} />
+      </main>
+      <footer>
+        <p>© 2023 Кінотеатр "Реакт". Всі права захищені.</p>
+      </footer>
     </div>
   );
 }
