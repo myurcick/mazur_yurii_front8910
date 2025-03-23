e0740f72ef9d0dc77263d04768392430b6b0af94
Мазур Юрій bc2024-1 <yurii.mazur@lnu.edu.ua>
2025-03-23 20:52:03 +0200

    fix: updatet images and view in page

--- cinema_proj/src/data/movies.js
+++ cinema_proj/src/data/movies.js
@@ -3,7 +3,7 @@ export const movies = [
     {
       id: 1,
       title: "Інтерстеллар",
-      image: "https://i.ibb.co/C6BbkGc/interstellar.jpg",
+      image: "https://www.movieposters.com/cdn/shop/files/interstellar-139399_480x.progressive.jpg?v=1708527823.webp",
       description: "Команда дослідників подорожує через космічну діру хробака, щоб забезпечити виживання людства.",
       genre: "Наукова фантастика",
       date: "2023-03-25",
@@ -12,7 +12,7 @@ export const movies = [
     {
       id: 2,
       title: "Початок",
-      image: "https://i.ibb.co/kQL6NvN/inception.jpg",
+      image: "https://www.movieposters.com/cdn/shop/files/inception.mpw.123395_9e0000d1-bc7f-400a-b488-15fa9e60a10c_480x.progressive.jpg?v=1708527589/poch.jpg",
       description: "Злодій, який викрадає корпоративні секрети через технологію обміну снами, отримує завдання імплантувати ідею у свідомість генерального директора.",
       genre: "Трилер",
       date: "2023-03-26",
@@ -21,7 +21,7 @@ export const movies = [
     {
       id: 3,
       title: "Зелена книга",
-      image: "https://i.ibb.co/BLRpKyT/green-book.jpg",
+      image: "https://cdn.shopify.com/s/files/1/0057/3728/3618/files/scan_d9bb976c-7c82-4618-9803-575a0681ec46_500x749.jpg?v=1729177999",
       description: "Працюючий викидайлом італо-американець стає водієм афроамериканського класичного піаніста на гастролях.",
       genre: "Драма",
       date: "2023-03-27",
@@ -30,7 +30,7 @@ export const movies = [
     {
       id: 4,
       title: "Паразити",
-      image: "https://i.ibb.co/QdZVRQG/parasite.jpg",
+      image: "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/parasite-24x36_500x749.jpg?v=1590609910",
       description: "Незаможна родина хитрістю влаштовується прислугою в будинок багатої родини, що призводить до неочікуваних наслідків.",
       genre: "Трилер/Драма",
       date: "2023-03-28",
@@ -39,7 +39,7 @@ export const movies = [
     {
       id: 5,
       title: "Джокер",
-      image: "https://i.ibb.co/tzWMzwV/joker.jpg",
+      image: "https://www.movieposters.com/cdn/shop/products/joker_th6ifhph_480x.progressive.jpg?v=1662670525",
       description: "У Готемі комік-невдаха з проблемами психічного здоров'я перетворюється на кримінального босса.",
       genre: "Кримінальний/Драма",
       date: "2023-03-29",
